818535b8f2e591911228f6ff441ab377faa23891
Hoang <hoang312tnhn@gmail.com>
2021-02-20 16:41:59 +0700

    khoi tao cac trang trangtru,laptop,dienthoai,giohang,thongtin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port=3000
 app.set('view engine', 'pug')
 
 app.get('/',(req,res)=>{
-	res.send('Day la trang tru')
+	res.render('trangtru',{})
 })
 app.listen(port,()=>{
 	console.log(`Ket noi thanh cong den cong ${port}`)
