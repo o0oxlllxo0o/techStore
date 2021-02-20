a2d668cb6425efbc0fa62d42d3831713ae08730c
Hoang <hoang312tnhn@gmail.com>
2021-02-20 16:36:53 +0700

    Them view engine pug

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express=require('express')
 const app=express()
 const port=3000
 
+app.set('view engine', 'pug')
+
 app.get('/',(req,res)=>{
 	res.send('Day la trang tru')
 })
-app.listen(port,(){
+app.listen(port,()=>{
 	console.log(`Ket noi thanh cong den cong ${port}`)
 })
